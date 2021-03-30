2e9e5734c323a9f0c70dbf70ef01e19d70568457
Patryk <patryk.piecek29@gmail.com>
2021-03-30 22:16:52 +0200

    feat(short_url): Add fields to django admin view

--- short_urls/admin.py
+++ short_urls/admin.py
@@ -6,6 +6,7 @@ from short_urls.models import ShortUrl
 @admin.register(ShortUrl)
 class ShortUrlAdmin(admin.ModelAdmin):
     list_display = ['original_url', 'short_url']
+    readonly_fields = ['short_path', 'original_url', 'short_url']
     model = ShortUrl
 
     class Meta:
